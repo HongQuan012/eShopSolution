53686263676cf6ea34b74313a948255193e90868
thuongthien07 <thuongthien07@gmail.com>
2020-04-21 21:05:21 +0700

    Add Entity

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 ## Technologies
 	+ ASP.NET Core 3.1
 	+ Entity FrameWork Core 3.1
+## install Packages
+	- Microsoft.EntityFrameworkCore.SqlServer
+	- Microsoft.EntityFrameworkCore.Design
+	- Microsoft.EntityFrameworkCore.Tools
 ## Youtube tutorial
 ## How to configure and run
 ## How to contribute
---
+++ eShopSolution.Data/Entities/Category.cs
@@ -0,0 +1,20 @@
+﻿using eShopSolution.Data.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Category
+    { //set ,get cho phép vừa đọc vừa ghi
+        public int Id { get; set; }
+
+        public int SortOrder { get; set; }
+
+        public bool IsShowOnHome { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
