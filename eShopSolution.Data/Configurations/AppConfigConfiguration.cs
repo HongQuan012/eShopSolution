c62050ad38656a174435599f6f78f683739c965c
thuongthien07 <thuongthien07@gmail.com>
2020-04-25 23:04:19 +0700

    config entity with Fluent API

---
+++ eShopSolution.Data/Configurations/AppConfigConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class AppConfigConfiguration : IEntityTypeConfiguration<AppConfig>
+    {
+        public void Configure(EntityTypeBuilder<AppConfig> builder)
+        {
+            //Tên bảng
+            builder.ToTable("AppConfigs");
+            //Có khóa chính
+            builder.HasKey(x => x.Key);
+            //bắt buộc phải nhập
+            builder.Property(x => x.Value).IsRequired(true);
+        }
+    }
+}
---
+++ eShopSolution.Data/Entities/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Order
+    {
+        public int Id { set; get; }
+
+        public string OrderDate { set; get; }
+
+        public string UserId { set; get; }
+
+        public string ShipName { set; get; }
+
+        public string ShipAddress { set; get; }
+
+        public string ShipEmail { set; get; }
+
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
