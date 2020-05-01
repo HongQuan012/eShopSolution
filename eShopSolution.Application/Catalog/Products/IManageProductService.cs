55b16f10ccbd3c109db7e9649434ad1aed3ff6b7
thuongthien07 <thuongthien07@gmail.com>
2020-05-01 15:44:40 +0700

    service

---
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,24 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos.Manage;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IManageProductService
+    {
+        //input là request , trả về kiểu int tức là mã sản phẩn vừa tạo
+        Task<int> Create(ProductCreateRequest request);
+        Task<int> Update(ProductUpdateRequest request);
+        Task<int> Delete(int productId);
+
+        Task<bool> UpdatePrice(int productid,decimal newPrice);
+        Task AddViewCount(int productId);
+        Task<bool> UpdateStock(int productId , int addedQuantity);
+
+        Task<List<ProductViewModel>> GetAll();
+        Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request);
+    }
+}
